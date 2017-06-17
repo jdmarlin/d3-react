2f96496b459cd238a09ed88e54c7736b08325cda
leejaed <johnlee@Johns-MacBook-Pro.local>
2017-06-17 01:58:53 -0400

    Updated index.js

--- pie-chart/index.js
+++ pie-chart/index.js
@@ -1,2 +1,21 @@
-import Chart from './chart'
-export default Chart
+import React, { Component } from 'react';
+import Chart from './pie-chart';
+import * as d3 from 'd3';
+
+export default class LineChart extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div ref={ref => this.chart = ref} >
+                <Chart
+                    chart={this.chart}
+                    height={this.props.height}
+                    data={this.props.data}
+                    keys={this.props.keys} />
+            </div>
+        )
+    }
+}
